9b7a3c1b0433076c978ffeb78a6e2db37b45a48f
Drilon Mecuku <dmecuku@Drilons-MacBook-Pro.local>
2019-07-24 16:54:15 +0100

    remove commented lines

--- a-practice/roman-numerals/roman_num_test.go
+++ a-practice/roman-numerals/roman_num_test.go
@@ -32,33 +32,3 @@ func TestRomanNumerals(t *testing.T) {
 	}
 }
 
-//Test v2
-// func TestRomanNumerals(t *testing.T) {
-// 	t.Run("1 gets converted to I", func(t *testing.T) {
-// 		got := ConvertToRoman(1)
-// 		want := "I"
-
-// 		if got != want {
-// 			t.Errorf("got '%s', want '%s'", got, want)
-// 		}
-// 	})
-
-// 	t.Run("2 gets converted to II", func(t *testing.T) {
-// 		got := ConvertToRoman(2)
-// 		want := "II"
-
-// 		if got != want {
-// 			t.Errorf("got '%s', want '%s'", got, want)
-// 		}
-// 	})
-// }
-
-//Test v1
-// func TestRomanNumerals(t *testing.T) {
-// 	got := ConvertToRoman(1)
-// 	want := "I"
-
-// 	if got != want {
-// 		t.Errorf("got '%s', want '%s'", got, want)
-// 	}
-// }
