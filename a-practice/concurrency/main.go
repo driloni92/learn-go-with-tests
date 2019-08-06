9480b591d1b2a898d01cf4066ef47bf27b51a18c
Drilon Mecuku <dmecuku@Drilons-MacBook-Pro.local>
2019-08-06 10:46:43 +0100

    add concurrency exercise sample

---
+++ a-practice/concurrency/main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func main() {
+	go count("sheep")
+	go count("fish")
+	//time.Sleep(time.Second * 3)
+
+	fmt.Scanln()
+}
+
+func count(thing string) {
+	for i := 1; true; i++ {
+		fmt.Println(i, thing)
+		time.Sleep(time.Millisecond * 500)
+	}
+}
